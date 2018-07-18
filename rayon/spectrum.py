a1c2adfabe688b7c5e183a875d96bb375589c57d
stephan <stephan@RZ.org>
2018-07-18 13:40:46 +0200

    Fit peaks detected in qxy spectrum

--- rayon/spectrum.py
+++ rayon/spectrum.py
@@ -5,8 +5,10 @@ import os.path
 import numpy as np
 
 from scipy.signal import find_peaks, peak_prominences
+from lmfit import Model
 
 dir_raw_data = '../RAW-DATA'
+dir_proc = 'PROC-DATA'
 
 def load_data_1D(ID):
     """
@@ -85,7 +87,7 @@ def channel2qz(ID, channel):
     deg_per_channel = 0.012957  # Let's trust the logbook
     rad_per_channel = np.deg2rad(deg_per_channel)
     
-    # Get the offset angle by averaging all the gamma angles.
+    # Get the offset angle by averaging all the gamma angles.
     offset_angle = load_metadata(ID)[5].mean()  # Degree
     offset_angle = np.deg2rad(offset_angle)
 
@@ -110,7 +112,65 @@ def get_I_qz(ID, data_2D, indices):
     return qz, intensities
 
 
+def gaussian(x, amp, cen, width):
+    """width = FWHM of the gaussian centered at position cen"""
+    return amp * np.exp(-4*np.log(2)*(x-cen)**2 / width**2)
 
 
+def line(x, slope, intercept):
+    """a line background"""
+    return slope*x + intercept
 
 
+def fit_peak(data_1D, indx0):
+    """
+    Gaussian fit of the peak centered around the index indx0
+    obtained from the array get_peaks_data_1D
+    
+    Return
+    ------
+    position, width and amplitude of the peak 
+    """
+    x = data_1D[0,indx0-13:indx0+13]   #number of channels=13 to cover the peak range 
+    y = data_1D[1,indx0-13:indx0+13] 
+    cen = data_1D[0,indx0]
+    slope = (data_1D[1,indx0+13]-data_1D[1,indx0-13])/(data_1D[0,indx0+13]-data_1D[0,indx0-13])
+    intercept = data_1D[1,indx0-13]
+    y_to_fit = y - line(x-data_1D[0,indx0-13],slope,intercept)
+ 
+    mod = Model(gaussian) + Model(line)
+    pars = mod.make_params(amp=10., cen=cen, width=0.05, slope=0., intercept=10.)
+    result = mod.fit(y_to_fit, pars, x=x)
+    
+#    print(result.fit_report())
+#    import matplotlib.pyplot as pl    
+#    pl.plot(x,y,'ro')
+#    pl.plot(x, result.best_fit+line(x-data_1D[0,indx0-13],slope,intercept))
+#    pl.show()
+
+    return result.best_values['cen'], abs(result.best_values['width']), result.best_values['amp']
+    
+
+
+def fit_peaks_spectrum(ID, data_1D,indices, save=False):
+    """
+    Fit of all the peaks detected (by their channel indices) in the q_xy diagram
+
+    Return
+    ------
+    position, width and amplitude of the peak     
+    """
+    list_fitparam = []
+    
+    for indx0 in indices:
+        cen, width, amplitude = fit_peak(data_1D, indx0)
+        list_fitparam.append([cen, width, amplitude]) 
+        
+    array_fitparam = np.array(list_fitparam).reshape(len(indices),3)
+
+    if save:
+        datpath = os.path.join(dir_proc, ID + '-peaks-fit.txt')
+        np.savetxt(datpath, array_fitparam, fmt='%.3e %.3e %.3e')
+
+    
+    
--- run_processing.py
+++ run_processing.py
@@ -7,78 +7,92 @@ import os
 
 import numpy as np
 
-from rayon.plot import *
-from rayon.spectrum import *
+import rayon.plot as rpl
+import rayon.spectrum as rspc
 
 
-IDs = ('SIRIUS_2018_05_30_01565',
-       'SIRIUS_2018_05_30_01567',
+IDs = (
+#       'SIRIUS_2018_05_30_01565',
+#       'SIRIUS_2018_05_30_01567',
        'SIRIUS_2018_05_30_01569',
-       'SIRIUS_2018_05_30_01571',
-       'SIRIUS_2018_05_30_01573',
-       'SIRIUS_2018_05_30_01575',
-       'SIRIUS_2018_05_30_01579',
-       'SIRIUS_2018_05_31_01585',
-       'SIRIUS_2018_05_31_01587',
-#       'SIRIUS_2018_05_31_01591', Missing metadata!
-       'SIRIUS_2018_05_31_01593',
-       'SIRIUS_2018_05_31_01595',
-       'SIRIUS_2018_05_31_01597',
-       'SIRIUS_2018_05_31_01599',
-       'SIRIUS_2018_05_31_01601',
-       'SIRIUS_2018_06_01_01641',
-       'SIRIUS_2018_06_01_01643',
-       'SIRIUS_2018_06_01_01647',
-       'SIRIUS_2018_06_01_01649',
-       'SIRIUS_2018_06_01_01654',
-#       'SIRIUS_2018_06_01_01656',
-       'SIRIUS_2018_06_01_01663',
-       'SIRIUS_2018_06_01_01683',
-       'SIRIUS_2018_06_01_01685',
-       'SIRIUS_2018_06_01_01687',
-       'SIRIUS_2018_06_01_01697',
-       'SIRIUS_2018_06_01_01701',
-       'SIRIUS_2018_06_01_01703',
-       'SIRIUS_2018_06_01_01717',
-       'SIRIUS_2018_06_01_01719',
-       'SIRIUS_2018_06_01_01721',
-       'SIRIUS_2018_06_02_01725',
-       'SIRIUS_2018_06_02_01729',
-       'SIRIUS_2018_06_02_01733',
-       'SIRIUS_2018_06_02_01737',
-       'SIRIUS_2018_06_02_01741',
-       'SIRIUS_2018_06_02_01761',
-       'SIRIUS_2018_06_02_01773',
-       'SIRIUS_2018_06_02_01791',
-       'SIRIUS_2018_06_03_01807',
-       'SIRIUS_2018_06_03_01825',
-       'SIRIUS_2018_06_03_01829',
-       'SIRIUS_2018_06_03_01849',
-       'SIRIUS_2018_06_03_01853',
-       'SIRIUS_2018_06_03_01855',
-       'SIRIUS_2018_06_03_01857',
+#       'SIRIUS_2018_05_30_01571',
+#       'SIRIUS_2018_05_30_01573',
+#       'SIRIUS_2018_05_30_01575',
+#       'SIRIUS_2018_05_30_01579',
+#       'SIRIUS_2018_05_31_01585',
+#       'SIRIUS_2018_05_31_01587',
+##       'SIRIUS_2018_05_31_01591', Missing metadata!
+#       'SIRIUS_2018_05_31_01593',
+#       'SIRIUS_2018_05_31_01595',
+#       'SIRIUS_2018_05_31_01597',
+#       'SIRIUS_2018_05_31_01599',
+#       'SIRIUS_2018_05_31_01601',
+#       'SIRIUS_2018_06_01_01641',
+#       'SIRIUS_2018_06_01_01643',
+#       'SIRIUS_2018_06_01_01647',
+#       'SIRIUS_2018_06_01_01649',
+#       'SIRIUS_2018_06_01_01654',
+##       'SIRIUS_2018_06_01_01656',
+#       'SIRIUS_2018_06_01_01663',
+#       'SIRIUS_2018_06_01_01683',
+#       'SIRIUS_2018_06_01_01685',
+#       'SIRIUS_2018_06_01_01687',
+#       'SIRIUS_2018_06_01_01697',
+#       'SIRIUS_2018_06_01_01701',
+#       'SIRIUS_2018_06_01_01703',
+#       'SIRIUS_2018_06_01_01717',
+#       'SIRIUS_2018_06_01_01719',
+#       'SIRIUS_2018_06_01_01721',
+#       'SIRIUS_2018_06_02_01725',
+#       'SIRIUS_2018_06_02_01729',
+#       'SIRIUS_2018_06_02_01733',
+#       'SIRIUS_2018_06_02_01737',
+#       'SIRIUS_2018_06_02_01741',
+#       'SIRIUS_2018_06_02_01761',
+#       'SIRIUS_2018_06_02_01773',
+#       'SIRIUS_2018_06_02_01791',
+#       'SIRIUS_2018_06_03_01807',
+#       'SIRIUS_2018_06_03_01825',
+#       'SIRIUS_2018_06_03_01829',
+#       'SIRIUS_2018_06_03_01849',
+#       'SIRIUS_2018_06_03_01853',
+#       'SIRIUS_2018_06_03_01855',
+#       'SIRIUS_2018_06_03_01857',
       )
+
 
 
 def task(ID):
     print(ID)
     # Load data 1D and 2D
-    data_1D = load_data_1D(ID)
-    data_2D = load_data_2D(ID)
+    data_1D = rspc.load_data_1D(ID)
+#    data_2D = rspc.load_data_2D(ID)
     # Detect peaks
-    peaks1D_idx = get_peaks_data_1D(data_1D)
-    # Get Profiles
-    I_qz = get_I_qz(ID, data_2D, peaks1D_idx)
+    peaks1D_idx = rspc.get_peaks_data_1D(data_1D)
+ 
+    # Fit peaks detected
+    rspc.fit_peaks_spectrum(ID, data_1D, peaks1D_idx,save=True)
 
-    plot_peaks_data_1D(ID, data_1D, peaks1D_idx, save=True)
-    plot_2D_map(ID, data_1D, data_2D, cmap='plasma', save=True)
-    plot_2D_map(ID, data_1D, data_2D, cmap='gray', save=True)
-    plot_I_qz_peaks(ID, data_1D, I_qz, peaks1D_idx, save=True)
-    plot_I_q_norm(ID, data_1D, data_2D, save=True)
-    plot_3D(data_1D, data_2D, ID, save=True)
+    # Get Profiles
+#    I_qz = rspc.get_I_qz(ID, data_2D, peaks1D_idx)
+
+#    rpl.plot_peaks_data_1D(ID, data_1D, peaks1D_idx, save=True)
+#    rpl.plot_2D_map(ID, data_1D, data_2D, cmap='plasma', save=True)
+#    rpl.plot_2D_map(ID, data_1D, data_2D, cmap='gray', save=True)
+#    rpl.plot_I_qz_peaks(ID, data_1D, I_qz, peaks1D_idx, save=True)
+#    rpl.plot_I_q_norm(ID, data_1D, data_2D, save=True)
+#    rpl.plot_3D(data_1D, data_2D, ID, save=True)
+
+#    import matplotlib.pyplot as pl
+#    pl.plot(data_1D[0,64:90],data_1D[1,64:90])
+#    pl.show()
 
 dir_plot = 'PLOTS'
 os.makedirs(dir_plot, exist_ok=True)
+dir_proc = 'PROC-DATA'
+os.makedirs(dir_proc, exist_ok=True)
+
+
 
 for ID in IDs:
     task(ID)
